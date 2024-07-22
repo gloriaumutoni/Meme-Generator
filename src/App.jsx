f64799eee35816b6f0c6704d326b1866e641b220
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-07-22 15:08:41 +0200

    meme generator

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./header"
+
+
+function App() {
+
+  return (
+    <div>
+      <div className="w-[40%] mx-auto">
+      <Header/>
+    </div>
+    </div>
+    
+  )
+}
+
+export default App
---
+++ src/header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <div className="py-2 px-3 bg-gradient-to-r from-[#672280] to-[#A626D3] flex justify-between">
+      <div className="flex">
+        <img src="/assets/Troll Face.svg" alt="" />
+        <h2>Meme Generator</h2>
+      </div>
+      <h4>React Course - Project 3</h4>
+    </div>
+  );
+};
+
+export default Header;
